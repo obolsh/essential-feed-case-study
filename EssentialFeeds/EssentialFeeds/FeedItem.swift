ea3d0263a15fd6e1058a5d6fa6a0984bc0df1f57
Oleksii Bolshakov <oleksii.bolshakov@nexar.app>
2023-08-08 10:02:17 +0300

    Add FeedItem

---
+++ EssentialFeeds/EssentialFeeds/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeeds
+//
+//  Created by Oleksii Bolshakov on 08.08.2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
