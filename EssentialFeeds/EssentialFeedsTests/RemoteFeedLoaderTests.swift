521ffc48da55f59af5fa7d4ed4dea44ac0e2f85c
Oleksii Bolshakov <oleksii.bolshakov@nexar.app>
2023-08-25 23:41:21 +0300

    Validate response status code and body

--- EssentialFeeds/EssentialFeedsTests/RemoteFeedLoaderTests.swift
+++ EssentialFeeds/EssentialFeedsTests/RemoteFeedLoaderTests.swift
@@ -51,7 +51,8 @@ final class RemoteFeedLoaderTests: XCTestCase {
         samples.enumerated().forEach{ index, code in
 
             expect(sut, completeWithResult: .failure(.invalidData), when: {
-                client.complete(withStatusCode: code, at: index)
+                let json = makeItemsJSON([])
+                client.complete(withStatusCode: code, data: json, at: index)
             })
         }
     }
@@ -152,7 +153,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
 
-        func complete (withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
+        func complete (withStatusCode code: Int, data: Data, at index: Int = 0) {
             let urlResponse = HTTPURLResponse(
                 url: messages[index].url,
                 statusCode: code,
