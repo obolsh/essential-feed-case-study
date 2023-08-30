57ee509631feced92e13b0cadd66e8f302b85bfb
Oleksii Bolshakov <oleksii.bolshakov@nexar.app>
2023-08-30 22:39:50 +0300

    Remove irrelevant test for a scope as already verified by other test

--- EssentialFeeds/EssentialFeedsTests/Feed Api/URLSessionHTTPClientTests.swift
+++ EssentialFeeds/EssentialFeedsTests/Feed Api/URLSessionHTTPClientTests.swift
@@ -24,25 +24,12 @@ class URLSessionHTTPClient {
 
 final class URLSessionHTTPClientTests: XCTestCase {
 
-  func test_getFromURL_createsDataTaskWithURL() {
-    let url = URL(string: "https://any-url.com")!
-
-    let session = URLSessionSpy()
-
-    let sut = URLSessionHTTPClient(session: session)
-    sut.get(from: url)
-
-    XCTAssertEqual(session.receivedURLs, [url])
-  }
-
   func test_getFromURL_resumesDataTaskWithURL() {
     let url = URL(string: "https://any-url.com")!
-
     let session = URLSessionSpy()
     let task = URLSessionDataTaskSpy()
 
     session.stub(url: url, task: task)
-
     let sut = URLSessionHTTPClient(session: session)
     sut.get(from: url)
 
@@ -50,7 +37,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
   }
 
   private class URLSessionSpy: URLSession {
-    var receivedURLs = [URL]()
     private var stubs = [URL : URLSessionDataTask] ()
 
     func stub(url: URL, task: URLSessionDataTask) {
@@ -58,7 +44,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
 
     override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-      receivedURLs.append(url)
       return stubs[url] ?? FakeURLSessionDataTask()
     }
   }
